1c293081c5075fc30e79380ba79ba14e45e446a0
Olivia <okarney@udel.edu>
2024-05-29 23:28:42 -0400

    hoping for deployment

---
+++ src/Components/Projects.tsx
@@ -0,0 +1,15 @@
+import '../App.css';
+import '../App';
+//import { Link } from "react-router-dom";
+//import { useNavigate } from 'react-router-dom';
+
+function Projects() {
+ 
+    
+  return (
+
+    <span>Projects go here</span>
+
+  );
+}
+export default Projects;
