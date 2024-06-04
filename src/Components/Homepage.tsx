28fe284ad78f25724b06852d066823a3e2f7076a
Olivia <okarney@udel.edu>
2024-06-03 21:24:42 -0400

    added some code for a header

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -1,14 +1,44 @@
 import '../App.css';
 import '../App';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 //import { Link } from "react-router-dom";
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Homepage() {
+  const navigate = useNavigate();
  
+  const goToAbout = () => {
+
+      // This will navigate to second component
+      navigate('/About');
+  };
+  const goToProjects = () => {
+
+      // This will navigate to first component
+      navigate('/Projects');
+  };
+
+  // the following lines were moved from the "Our Header Function"
+
+const goToHome = () => {
+
+  // This will navigate to second component
+  navigate('/Homepage');
+};
+
     
   return (
+    <div>
+        <span>Homepage</span>
 
-    <span>Homepage</span>
+        <div>
+          <ul>
+            <li><Button className="BasicButton" onClick={goToHome}> Home </Button></li>
+            <li><Button className="BasicButton" onClick={goToAbout}> About </Button></li>
+            <li><Button className="BasicButton" onClick={goToProjects}> Survey </Button></li>      
+          </ul>
+        </div>
+      </div>
 
   );
 }
